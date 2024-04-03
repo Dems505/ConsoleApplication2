066a3bee6ea2207c25e60739e386979d396d7e1c
dems 1 <galkmat@hotmail.com>
2024-04-03 20:04:28 +0300

    Создано решение

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,20 +1,39 @@
-﻿// ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
 
-#include <iostream>
+void FindOddNumbers(int Limit, bool IsOdd)
+{
+	for (int i = 1; i <= Limit; i++) {
+		if (IsOdd == true && i % 2 != 0) {
+			std::cout << i << "\n";
+		}
+		else if (IsOdd == false && i % 2 == 0) {
+			std::cout << i << "\n";
+		}
+	}
+};
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int variable, isOdd;
+	std::cout << "enter a variable: ";
+	std::cin >> variable;
+	std::cout << "which numbers should be even or not (1 odd , 2 even): ";
+	std::cin >> isOdd;
+	for (int i = 1; i <= variable; i++)
+		{
+		std::cout << i << "\n";
+		}
+	if (isOdd == 1) {
+		std::cout << "\n";
+		FindOddNumbers(variable, true);
+	}
+	else if (isOdd == 2) {
+		std::cout << "\n";
+		FindOddNumbers(variable, false);
+	}
+	else {
+		std::cout << "To print even or odd numbers, enter the correct number!";
+	}
+	
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
